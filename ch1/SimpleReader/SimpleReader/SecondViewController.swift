8b17f5869f0a388cbfb10ebda120c976026bea70
Liyao Chen <gliyao@gmail.com>
2014-10-03 10:18:59 +0800

    first commit

---
+++ ch1/SimpleReader/SimpleReader/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SimpleReader
+//
+//  Created by Liyao on 2014/10/3.
+//  Copyright (c) 2014年 HaveFun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
